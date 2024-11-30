c27342b52d3771bfaf28564b082042a413f012b7
Manuel Stoeckl <code@mstoeckl.com>
2024-11-30 12:45:15 -0500

    Autoformat meson build files

--- autoformat.sh
+++ autoformat.sh
@@ -4,3 +4,4 @@ cargo fmt
 ruff format -q -n protogen.py waypipe-c/test/*.py waypipe-c/protocols/*.py
 clang-format -style=file --assume-filename=C -i waypipe-c/*.h waypipe-c/*.c waypipe-c/test/*.c waypipe-c/test/*.h
 clang-format -style=llvm -i shaders/*.glsl
+meson fmt -i meson.build waypipe-c/meson.build waypipe-c/protocols/meson.build waypipe-c/test/meson.build
