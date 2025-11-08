7e104fff179c5b565424b88522cf11a36b0cbdf1
Manuel Stoeckl <code@mstoeckl.com>
2025-11-08 07:56:00 -0500

    Silence missing docs lints on generated code

--- wrap-zstd/src/lib.rs
+++ wrap-zstd/src/lib.rs
@@ -1,8 +1,10 @@
+#![allow(missing_docs)]
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(improper_ctypes)] // u128 FFI should not be used
 #![allow(rustdoc::invalid_html_tags)]
 #![allow(rustdoc::broken_intra_doc_links)]
+#![allow(clippy::missing_safety_doc)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
